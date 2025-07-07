0dc22570efeedd56a6150daab081307a1a7a1dfe
Atharv <105142090+atharrvv@users.noreply.github.com>
2025-07-07 14:17:08 +0530

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,10 @@
-def test_sample():
-    assert 1 + 1 == 2
+# tests/test_math.py
+
+def test_addition():
+    assert 2 + 3 == 5
+
+def test_subtraction():
+    assert 5 - 2 == 3
+
+def test_failure():
+    assert 2 * 2 == 5  # This will fail
